f95ca190986f272d98bf78dae109836b5b5a7e2f
JIeJaitt <498938874@qq.com>
2023-05-16 17:30:05 +0800

    feat: improve api/dbops of AddUserCredential, GetUserCredential and DeleteUser

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -2,12 +2,45 @@ package dbops
 
 import (
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func AddUserCredential(loginName string, pwd string) error {
-
+	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+	stmtIns.Exec(loginName, pwd)
+	// defer 是在栈退出的时候才会调用
+	// 所以 defer 对性能有些许损耗
+	// 尽量少在对性能要求严格的项目中使用 defer
+	stmtIns.Close()
+	return nil
 }
 
 func GetUserCredential(loginName string) (string, error) {
+	stmtOut, err := dbConn.Prepare("SELECT pwd FROM users WHERE login_name = ?")
+	if err != nil {
+		log.Printf("%s", err)
+		// string 是没有 nil 的, 所以这里返回一个空字符串
+		// nil 大多数时候是出现在 pointer, interface, map, slice, channel, function 这些类型上
+		return "", err
+	}
 
+	var pwd string
+	stmtOut.QueryRow(loginName).Scan(&pwd)
+	stmtOut.Close()
+	return pwd, nil
+}
+
+func DeleteUser(loginName string, pwd string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM users WHERE login_name = ? AND pwd = ?")
+	if err != nil {
+		log.Printf("DeleteUser error: %s", err)
+		return err
+	}
+
+	stmtDel.Exec(loginName, pwd)
+	stmtDel.Close()
+	return nil
 }
