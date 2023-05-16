ad7732ca25a80f5a95063a26124d17e350e928c3
JIeJaitt <498938874@qq.com>
2023-05-16 18:48:51 +0800

    feat: add VideoInfo struct and methods

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"stmsrv/api/defs"
+	"stmsrv/api/utils"
+	"time"
 )
 
 func AddUserCredential(loginName string, pwd string) error {
@@ -46,7 +49,7 @@ func GetUserCredential(loginName string) (string, error) {
 	}
 
 	var pwd string
-	stmtOut.QueryRow(loginName).Scan(&pwd)
+	err = stmtOut.QueryRow(loginName).Scan(&pwd)
 	// ErrNoRows 实际上并不是一个真正的处理错误
 	// 而是没有结果
 	// 而这里如果不处理程序会把 NoRows 按照错误来返回
@@ -70,6 +73,96 @@ func DeleteUserCredential(loginName string, pwd string) error {
 	}
 
 	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+	defer func(stmtDel *sql.Stmt) {
+		err := stmtDel.Close()
+		if err != nil {
+			return
+		}
+	}(stmtDel)
+	return nil
+}
+
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	// create uuid
+	vid, err := utils.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	// create time -> db ->
+	// 真正的写库时间和生成视频的时间是有区别的
+
+	t := time.Now()
+	ctime := t.Format("Jan 02 2006, 15:04:05")
+
+	stmtIns, err := dbConn.Prepare(`INSERT INTO video_info 
+		(id, author_id, name, display_ctime) VALUES(?, ?, ?, ?)`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmtIns.Exec(vid, aid, name, ctime)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(stmtIns *sql.Stmt) {
+		err := stmtIns.Close()
+		if err != nil {
+			return
+		}
+	}(stmtIns)
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: ctime}
+	return res, nil
+}
+
+type UserError struct {
+	Message string
+}
+
+func (e *UserError) Error() string {
+	return e.Message
+}
+
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	stmtOut, err := dbConn.Prepare(`SELECT author_id, name, display_ctime FROM video_info 
+		WHERE id = ?`)
+	if err != nil {
+		return nil, err
+	}
+
+	var aid int
+	var dct string
+	var name string
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+
+	if err == sql.ErrNoRows {
+		return nil, &UserError{"User not found"}
+	}
+	if err != nil {
+		return nil, &UserError{"Error retrieving user"}
+	}
+	defer func(stmtOut *sql.Stmt) {
+		err := stmtOut.Close()
+		if err != nil {
+			return
+		}
+	}(stmtOut)
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: dct}
+	return res, nil
+}
+
+func DeleteVideoInfo(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_info WHERE id = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtDel.Exec(vid)
 	if err != nil {
 		return err
 	}
--- api/defs/apidef.go
+++ api/defs/apidef.go
@@ -1,6 +1,13 @@
 package defs
 
-type UserCredentail struct {
+type UserCredential struct {
 	Username string `json:"user_name"`
 	Pwd      string `json:"pwd"`
+}
+
+type VideoInfo struct {
+	Id           string
+	AuthorId     int
+	Name         string
+	DisplayCtime string
 }
