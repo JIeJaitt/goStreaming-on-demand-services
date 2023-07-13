0063c6db7f0379b8699ac245521b7f9d6e727da7
JIeJaitt <498938874@qq.com>
2023-07-13 23:29:57 +0800

    feat: Overview of Streaming Server Architecture
    Closes #11

---
+++ streamserver/handlers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Param) {
+
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Param) {
+
+}
---
+++ streamserver/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func RegisterHandler() *httprouter.Router {
+	router := httprouter.New()
+	router.GET("/videos/:vid-id", streamHandler)
+	router.POST("/upload/:vid-id", uploadHandler)
+	return router
+}
+
+func main() {
+	r := RegisterHandler()
+	http.ListenAndServe(":9000", r)
+}
