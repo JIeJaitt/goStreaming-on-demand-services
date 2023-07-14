393aa43a42230f0002426e32071b724abbb0777c
JIeJaitt <498938874@qq.com>
2023-07-14 15:59:27 +0800

    feat: Embedding Flow Control in HTTP Middleware
    Closes #14

--- streamserver/main.go
+++ streamserver/main.go
@@ -5,6 +5,27 @@ import (
 	"net/http"
 )
 
+type middleWareHandler struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+func NewMiddleWareHandler(r *httprouter.Router, cc int) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	m.l = NewConnLimiter(cc)
+	return m
+}
+
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !m.l.GetConn() {
+		sendErrorResponse(w, http.StatusTooManyRequests, "Too many requests.")
+		return
+	}
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+}
+
 func RegisterHandler() *httprouter.Router {
 	router := httprouter.New()
 	router.GET("/videos/:vid-id", streamHandler)
@@ -14,5 +35,6 @@ func RegisterHandler() *httprouter.Router {
 
 func main() {
 	r := RegisterHandler()
-	http.ListenAndServe(":9000", r)
+	mh := NewMiddleWareHandler(r, 2)
+	http.ListenAndServe(":9000", mh)
 }
