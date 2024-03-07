9352922245c7423f80f122631ae4e8af8ffbbf46
JIeJaitt <498938874@qq.com>
2024-03-07 21:11:59 +0800

    feat: implement timer functionality
    Closes #23

--- scheduler/taskrunner/trmain.go
+++ scheduler/taskrunner/trmain.go
@@ -1,1 +1,39 @@
 package taskrunner
+
+import (
+	"log"
+	"time"
+)
+
+// Timer部分
+// setup -> start { trigger -> task -> runner } -> close
+
+type Worker struct {
+	ticket *time.Ticker // 定时器
+	runner *Runner      // 任务执行器
+}
+
+func NewWorker(interval time.Duration, r *Runner) *Worker {
+	return &Worker{
+		ticket: time.NewTicker(interval * time.Second),
+		runner: r,
+	}
+}
+
+func (w *Worker) startWorker() {
+	for {
+		select {
+		// 定时器到了
+		case <-w.ticket.C:
+			log.Printf("ticket run start--------------------\n")
+			go w.runner.StartAll()
+			log.Printf("ticket run end--------------------\n")
+		}
+	}
+}
+
+func Start() {
+	r := NewRunner(3, true, VideoClearDispatcher, VideoClearExecutor)
+	w := NewWorker(3, r)
+	go w.startWorker()
+}
